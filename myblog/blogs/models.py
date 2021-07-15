b2382155a17937765c15eb29e320febe282d639f
Noknot2810 <Artem15012001@mail.ru>
2021-07-15 20:20:43 +0400

    Models was created and first samples was added in the DB

--- myblog/blogs/models.py
+++ myblog/blogs/models.py
@@ -1,3 +1,27 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Blog(models.Model):
+
+    def __str__(self):
+        return self.title
+
+    title = models.CharField(max_length=200)
+    art_count = models.IntegerField(default=0)
+
+
+class Article(models.Model):
+    def __str__(self):
+        return self.headline
+
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    headline = models.CharField(max_length=200)
+    text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    image = models.ImageField(null=True, upload_to="images/")
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
